28fdb6da6d57a05e82efb377bf2c069ab0e43a23
aalvesnas <82824049+aalvesnas@users.noreply.github.com>
2022-11-30 13:28:17 -0300

    curriculum vitae

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  document.getElementById("objetivo").innerHTML = "Desenvolvedor Front End Jr";
+}
+
+function myFunctionContact() {
+  document.getElementById("contato").innerHTML =
+    "<br>Rua Pampa, 191 – Jabaquara – São Paulo / SP – Região Sul <br></br>Tel: (11) 98438-0432 / Email: aalvesnas@gmail.com<br></br>";
+}
+
+function myFunctionProfile() {
+  document.getElementById("perfil").innerHTML =
+    "<br>Desenvolvedor Front End iniciante, estou buscando uma oportunidade para ingressar na área de TI.<br></br>Tenho desenvolvido habilidades nas seguintes linguagens de programação: HTML5, CSS3, JavaScript, além de noção dos frameworks React e Angular.<br></br>Também possuo experiência com suporte de atendimento para aplicativos bancários.<br></br>";
+}
+
+function myFunctionFormation() {
+  document.getElementById("formacao").innerHTML =
+    "<br>UGF (RJ)  =>  Tecnólogo em Proc. de Petr. e Gás Nat.  =>   08/2004 – 12/2006<br></br>ETEC Lauro Gomes (SP)  =>  Técnico em Química  => 07/2012 – 07/2014<br></br>";
+}
+
+function myFunctionExperience() {
+  document.getElementById("experiencia").innerHTML =
+    "<br>LIQ => Analista de Atendimento => 10/2020 – 08/2022<br>•	Atendimento aos clientes usuários dos aplicativos de conta corrente, cartão de crédito e internet banking, para ajudar com os problemas do aplicativo (suporte), além de apoiar com procedimentos da conta corrente e cartão de crédito utilizando sistema próprio(CRM) entre outros como Salesforce.<br></br> Atento => Operador de Vendas I => 09/2017 – 09/2019<br>•	Oferta ativa e receptiva por telefone de serviços de antecipação de recebíveis de vendas e antecipação programada para departamentos financeiros de estabelecimentos comerciais, e para clientes empreendedores portadores de máquinas de cartão de crédito.<br></br> Veganet => Operador de Telemarketing => 10/2016 – 12/2016<br>•	Oferta ativa por telefone de serviço de DDD ilimitado para usuários de telefonia fixa, utilizando-se de venda de impacto para atingir metas.<br></br>BCR => Analista de Atendimento => 12/2015 – 04/2016<br>•	Atendimento receptivo para clientes de e-commerce de sistema de ensino para acompanhar ou efetuar cadastro de clientes; envio por e-mail de link para redefinição de senha de acesso a plataforma de pagamento, para acessar boletos, notas fiscais ou recibos.<br>•	Acompanhamento ou realização da compra do material didático para o cliente; prestação de informações tanto para o cliente como para a escola; alterações cadastrais entre outras solicitações.<br></br>";
+}
+
+function myFunctionCourses() {
+  document.getElementById("cursos").innerHTML =
+    "<br>UDEMY => Lógica de Programação com JavaScript => 2022<br></br>UDEMY => JavaScript do Básico ao Avançado (c/ Node.js e Projetos) => 2022<br></br> CODER => Angular 9 – Essencial => 2022<br></br>UDEMY => Como Aprender React em 1 Dia => 2022</br>";
+}
